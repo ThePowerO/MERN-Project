7c313f2896f2c7f97008983f5d7d09201d040c08
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-10-05 18:06:18 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,3 +6,5 @@ const { mongoose } = require('mongoose')
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Connected to database"))
 .catch((err) => console.log("Can not connect to database", err))
+
+const app = express();
