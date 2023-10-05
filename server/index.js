70c88de0e4478ce180365ec0dfb9c0b068ba1142
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-10-05 18:05:58 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,3 +2,5 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const { mongoose } = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URL)
