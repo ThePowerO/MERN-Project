f63a032030531daf16c1d00d8647401b3e5d31b7
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-10-06 17:30:51 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const { mongoose } = require('mongoose')
+const app = express();
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Connected to database"))
 .catch((err) => console.log("Can not connect to database", err))
 
-const app = express();
+// middleware
+app.use(express.json())
 
 app.use('/', require('./routes/authRoutes'))
 
